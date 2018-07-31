7f3d060970953a0a1084c70f8a54feabf7eb7a8c
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2018-07-31 22:26:40 +0200

    [endoflesson] add googlemaps map

--- src/components/chart.js
+++ src/components/chart.js
@@ -6,9 +6,8 @@ function average(data) {
     return _.round(_.sum(data)/data.length);
 }
 export default (props) => {
-    //chart Size is not working, so we specify the div size
     return(
-        <div style={{"height": "120px", "width": "180px"}}> 
+        <div> 
             <Sparklines height={120} width={180} data={props.data}>
                 <SparklinesLine color={props.color} />
                 <SparklinesReferenceLine type="avg" />
